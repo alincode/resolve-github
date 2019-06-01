6c2051e9513732bdf115a32f9cdae31e1ab6fc72
Ai-Lin Liou <alincode@gmail.com>
2019-06-02 00:33:37 +0800

    support symbolic link

--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,38 @@
-const replaceContent = require('solc-import').replaceContent;
-const resolver = require('./resolver');
+const replaceContent = require('solc-import').replaceContent
+const resolver = require('./resolver')
 // https://github.com/<owner>/<repo>/<path_to_the_file>
 
-module.exports = async function (importPath) {
-  const [, , , root, path] = require('./index').match.exec(importPath);
+module.exports = async function(importPath) {
+  const [, , , root, path] = require('./index').match.exec(importPath)
 
-  const url = `https://raw.githubusercontent.com/${root}/master/${path}`;
+  let url = `https://raw.githubusercontent.com/${root}/master/${path}`
   try {
-    const response = await fetch(url, { method: 'GET' });
-    let data = await response.text();
-    if (!response.ok || response.status !== 200) throw Error('Content ' + data);
-    data = replaceContent(data, importPath, resolver);
-    return data;
+    let data = await getData(url)
+    if (isSymbolicLink(data)) {
+      const tmps = url.split('/')
+      const filename = tmps[tmps.length - 1]
+      url = url.replace(filename, data)
+      data = await getData(url)
+    } else {
+      data = replaceContent(data, importPath, resolver)
+    }
+    return data
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
+async function getData(url) {
+  let response = await fetch(url, { method: 'GET' })
+  let data = await response.text()
+  if (!response.ok || response.status !== 200) throw Error('Content ' + data)
+  return data
+}
+
+function isSymbolicLink(data) {
+  if (data.length < 50 && data.indexOf('.sol')) return true
+  return false
+}
 
 // async function getSource(importPath, root, path) {
 //   const url = `https://api.github.com/repos/${root}/contents/${path}`;
--- test/index.js
+++ test/index.js
@@ -1,40 +1,55 @@
-require('solcjs-mock')();
+require('solcjs-mock')()
 
-const chai = require('chai');
-chai.should();
+const chai = require('chai')
+chai.should()
 
-const github = require('../src');
+const github = require('../src')
 
 describe('github', () => {
-
   it('found', async () => {
-    const path = 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol';
-    console.time('first time');
-    let content = await github.parser(path);
-    console.timeEnd('first time');
-    content.should.be.a('string');
-    content.length.should.be.above(50);
+    const path =
+      'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol'
+    console.time('first time')
+    let content = await github.parser(path)
+    console.timeEnd('first time')
+    content.should.be.a('string')
+    content.length.should.be.above(50)
 
     // get source from cache
-    console.time('second time');
-    await github.parser(path);
-    console.timeEnd('second time');
-  });
+    console.time('second time')
+    await github.parser(path)
+    console.timeEnd('second time')
+  })
+
+  it('symbolic link', async () => {
+    const path = 'https://github.com/oraclize/ethereum-api/oraclizeAPI.sol'
+    console.time('first time')
+    let content = await github.parser(path)
+    console.timeEnd('first time')
+    content.should.be.a('string')
+    content.length.should.be.above(50)
+
+    // get source from cache
+    console.time('second time')
+    await github.parser(path)
+    console.timeEnd('second time')
+  })
 
   it('no found', async () => {
-    const path = 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath2.sol';
+    const path =
+      'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath2.sol'
     try {
-      await github.parser(path);  
+      await github.parser(path)
     } catch (error) {
-      error.should.be.a('error');
-      error.message.should.be.eq('Content 404: Not Found\n');
+      error.should.be.a('error')
+      error.message.should.be.eq('Content 404: Not Found\n')
     }
-  });
+  })
 
   it('path resolve', async () => {
-    const path = 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/examples/SimpleToken.sol';
-    let content = await github.parser(path);
-    content.should.to.match(/OpenZeppelin/);
-  });
-
-});
+    const path =
+      'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/examples/SimpleToken.sol'
+    let content = await github.parser(path)
+    content.should.to.match(/OpenZeppelin/)
+  })
+})
